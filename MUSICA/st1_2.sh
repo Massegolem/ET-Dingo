79ff9216be0d1485811fc1003831b7336f51012b
Pausch <tpausch@hepgpu2.oeaw.ads>
2026-09-14 13:55:22 +0000

    m

---
+++ MUSICA/st1_2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=dingo_st1_2
+#SBATCH --account=p201368
+#SBATCH --partition=zen4_0768_h100x4
+#SBATCH --qos=zen4_0768_h100x4
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=22
+#SBATCH --time=25:00:00
+#SBATCH --output=st1_2.out
+
+module load Miniforge3/25.3.0-3
+eval "$(conda shell.bash hook)"
+conda activate $DATA/conda_envs/ET-DINGO
+
+python -c "import torch; print(torch.cuda.is_available(), torch.cuda.get_device_name(0))"
+time dingo_train --settings_file $DATA/ET-Dingo/ET-Dingo/MUSICA/train_settings_st1_2.yaml --train_dir $DATA/models/st1_2
